73c41ef1c2fdf345088d9a2c6578a761172736dc
Brad Aagaard <baagaard@usgs.gov>
2019-04-16 21:30:05 -0700

    Fix event/event_dict issue in processing (kludge). Minor cleanup.

--- presentations/2019-SSA-ASDF/work/demo.py
+++ presentations/2019-SSA-ASDF/work/demo.py
@@ -26,42 +26,47 @@ warnings.filterwarnings("ignore", category=H5pyDeprecationWarning)
 
 def data_import():
 
-    # Event file
-    eventfile = os.path.join(EVENTID, "event.json")
-    with open(eventfile, "r") as fin:
+    # Import earthquake metadata
+    eqfile = os.path.join(EVENTID, "event.json")
+    with open(eqfile, "r") as fin:
         event = get_event_object(json.load(fin))
 
+    # Import "raw" waveforms from CESMD
     datafiles = glob.glob(EVENTID+"/*.smc")
     raw_streams = []
     for dfile in datafiles:
         raw_streams += read_data(dfile)
 
+    # Import "raw" waveforms from NCEDC
     datafiles = glob.glob(EVENTID+"/*.mseed")
     for dfile in datafiles:
         raw_streams += read_fdsn(dfile)
 
+    # Write data to workspace
     workspace = StreamWorkspace(FILENAME)
     workspace.addStreams(event, raw_streams, label="raw")
 
     return
 
 
-def check_event():
-    import h5py
-    with h5py.File(FILENAME, "r") as h5:
-        xmlbytes = h5["QuakeML"][:].tostring().strip(b"\x00 ").strip()
-    print(xmlbytes)
-
-
 def process_waveforms():
 
+    # :KLUDE: process_streams() takes event dict not ObsPy Event
+    eventfile = os.path.join(EVENTID, "event.json")
+    with open(eventfile, "r") as fin:
+        event_dict = json.load(fin)
+
+    # Get raw waveforms from workspace    
     workspace = StreamWorkspace.open(FILENAME)
-    event = get_event_object(workspace.getEvent(EVENTID))
     raw_streams = workspace.getStreams(EVENTID, labels=["raw"])
-    processed_streams = process_streams(raw_streams, event, config=config)
-    workspace.addStreams(event, processed_streams, 'processed')
-    
-    worspace.setStreamMetrics(EVENTID, labels=["processed"], imclist=COMPONENT_METRICS, imtlist=INTENSITY_METRICS)
+    event = workspace.getEvent(EVENTID)
+
+    # Process waveforms
+    processed_streams = process_streams(raw_streams, event_dict, config=config)
+    workspace.addStreams(event, processed_streams, label="processed")
+
+    # Set waveform metrics in workspace
+    workspace.setStreamMetrics(EVENTID, labels=["processed"], imclist=COMPONENT_METRICS, imtlist=INTENSITY_METRICS)
     
     return
 
@@ -74,17 +79,16 @@ def query():
     print("Stream tags in workspace: {}".format(workspace.getStreamTags(EVENTID)))
     print("Processing labels in workspace: {}".format(workspace.getLabels()))
     
-    #stream_nc_n016 = workspace.getStreams(EVENTID, stations=["NC.N016"], labels="processed")
-    #stream_nc_n016.describe()
+    stream_nc_n016 = workspace.getStreams(EVENTID, labels="processed")
+    stream_nc_n016.describe()
 
-    #metrics_nc_n016 = workspace.getStreamMetrics(EVENTID, station="NC.N016", label=['processed'])
+    metrics_nc_n016 = workspace.getStreamMetrics(EVENTID, station="NC.N016", label=['processed'])
     return
 
 
 if __name__ == "__main__":
-    #data_import()
-    #check_event()
+    data_import()
     process_waveforms()
-    #query()
+    query()
     
     
